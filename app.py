1ab71c463c356b1bab607c31b24868560438f2ab
OsiasHab <osias@savanesprl.com>
2026-07-16 22:47:34 +0300

    GOOD

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
-    currency = db.Column(db.String(10), default='FCFA')
+    currency = db.Column(db.String(10), default='BIF')
     email = db.Column(db.String(120), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
